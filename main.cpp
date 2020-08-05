5cd6491031234bc8988dd82815172fbcf9c24f88
Nad3z <dan.orn.other@pm.me>
2020-08-05 19:23:49 +0000

    Updated the user interface to be more command-like

--- main.cpp
+++ main.cpp
@@ -1,70 +1,41 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
-std::string cipher(std::string input) {
-    std::string output;
-
-    for (int i = 0; i < input.length(); i++) {
-        if(input[i] >= 65 && input[i] <= 90) {
-            if (input[i] + 13 > 90)
-                output += 64 + (13 + (input[i] - 90));
-            else
-                output += input[i] + 13;
-        }
-        else if(input[i] >= 97 && input[i] <= 122) {
-            if (input[i] + 13 > 122)
-                output += 96 + (13 + (input[i] - 122));
-            else
-                output += input[i] + 13;
-        }
-        else
-            output += input[i];
-    }
-
-    return output;
-}
-
-std::string decipher(std::string input) {
-    std::string output;
-
-    for (int i = 0; i < input.length(); i++) {
-        if(input[i] >= 65 && input[i] <= 90) {
-            if (input[i] - 13 < 65)
-                output += 91 - (13 - (input[i] - 65));
-            else
-                output += input[i] - 13;
-        }
-        else if(input[i] >= 97 && input[i] <= 122) {
-            if (input[i] - 13 < 97)
-                output += 123 - (13 - (input[i] - 97));
-            else
-                output += input[i] - 13;
-        }
-        else
-            output += input[i];
-    }
-
-    return output;
-}
+#include "ROT13.h"
 
 int main() {
-    while(true) {
-        int choice;
-        std::cout << "0. Cipher\n1. Decipher\nEnter a number\n>>> ";
-        std::cin >> choice;
+    ROT13 rot13 = ROT13();
+    std::string command;
+    std::string input;
+    std::stringstream ss;
+    std::string text;
 
-        std::string input;
-        std::cout << "Enter a string\n>>> ";
-        std::cin.get();
+    std::cout << "Use the \"help\" command to see available commands." << std::endl;
+
+    while(command != "quit") {
+        command = "";
+        input = "";
+        ss.clear();
+
+        std::cout << ">>> ";
         getline(std::cin, input);
 
-        if (choice == 0)
-            std::cout << cipher(input) << std::endl;
-        else if(choice == 1)
-            std::cout << decipher(input) << std::endl;
+        ss << input;
+        ss >> command;
 
-        std::cout << "Press \"[ENTER]\" to run the program again or \"[CTRL] + [C]\" to quit.";
-        std::cin.get();
+        if(command == "c" or command == "cipher") {
+            ss >> text;
+            std::cout << rot13.cipher(text) << std::endl;
+        }
+        else if(command == "d" or command == "decipher") {
+            ss >> text;
+            std::cout << rot13.decipher(text) << std::endl;
+        }
+        else if(command != "quit") {
+            std::cout << "Unknown command!" << std::endl;
+        }
     }
+
     return 0;
 }
