7a5a96c52573d285f4068fc9fec1e905c9beb6d9
Nad3z <65785678+Nad3z@users.noreply.github.com>
2020-08-17 22:15:01 +0000

    Added sentence ciphering and corrected values

--- ROT13.cpp
+++ ROT13.cpp
@@ -1,25 +1,17 @@
 #include "ROT13.h"
 
-bool isCapital(char l) {
-    if(l >= 65 && l <= 90) {
-        return true;
-    }
-
-    return false;
-}
-
 std::string ROT13::cipher(std::string input, int variant) {
     std::string output;
 
     for(int i = 0; i < (int)input.length(); i++) {
         if(std::isdigit(input[i]) && (variant == 5 || variant == 18))
             output += input[i] >= 53 ? input[i] - 5 : input[i] + 5;
-        else if(isCapital(input[i]) && !std::isdigit(input[i]) && (variant == 13 || variant == 18))
+        else if((input[i] >= 65 && input[i] <= 90) && !std::isdigit(input[i]) && (variant == 13 || variant == 18))
             output += input[i] >= 78 ? input[i] - 13 : input[i] + 13;
-        else if(!isCapital(input[i]) && !std::isdigit(input[i]) && (variant == 13 || variant == 18))
+        else if((input[i] >= 97 && input[i] <= 122) && !std::isdigit(input[i]) && (variant == 13 || variant == 18))
             output += input[i] <= 109 ? input[i] + 13 : input[i] - 13;
-        else if(variant == 47)
-            output += input[i] >= 80 ? input[i] - 47 : input[i] + 47;
+        else if((input[i] >= 33 && input[i] <= 126) && variant == 47)
+            output += input[i] >= 79 ? input[i] - 47 : input[i] + 47;
         else
             output += input[i];
     }
